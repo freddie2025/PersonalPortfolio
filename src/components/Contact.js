95737800347171c229e553517c55a592408237e9
Fred James <fredjsam@live.co.uk>
2020-01-02 19:53:22 +0000

    Contact form logic

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,78 +1,106 @@
 import React, { Component } from 'react';
+import { navigate } from 'gatsby-link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import '../styles/contact.css'; 
 
-class Contact extends Component { 
-    render() {
-        return (
-            <div>
-                <span id="anchor-contact" className="section-spacing-anchor" />
-                <div id="contact" className="container">
-                    <div className="tile is-ancestor tile-margin-override">
-                        <div className="tile is-child box border-radius-override">
-                            <div className="content has-text-centered">
-                                <h1 className="title"><span className="shadow-is-yellow">Contact</span></h1>
-                            </div>
-                            <hr />
-                            <div className="container container-padding">
-                                <div className="columns">
-                                    <div className="column is-two-thirds">
-                                        <form asp-action="Index" asp-controller="Home" method="post">
-                                            <div className="field is-horizontal">
-                                                <div className="field-body">
-                                                    <div className="field">
-                                                        <p className="control has-icons-left">
-                                                            <input asp-for="Name" aria-label="Name" className="input border-radius-override" id="Name" name="Name" placeholder="Name" type="text"  />
-                                                            <span className="icon is-small is-left">
-                                                                <FontAwesomeIcon icon="user" />
-                                                            </span>
-                                                        </p>
-                                                    </div>
-                                                    <div className="field">
-                                                        <p className="control has-icons-left has-icons-right">
-                                                            <input asp-for="Email" aria-label="Email" className="input border-radius-override" id="Email" name="Email" placeholder="Email" type="text"  />
-                                                            <span className="icon is-small is-left">
-                                                                <FontAwesomeIcon icon="envelope" />
-                                                            </span>
-                                                        </p>
+function encode(data) {
+    return Object.keys(data)
+      .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&')
+  }
+
+export default function Contact() {
+    const [state, setState] = React.useState({})
+  
+    const handleChange = (e) => {
+        setState({ ...state, [e.target.name]: e.target.value })
+    }
+  
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const form = e.target
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: encode({
+                'form-name': form.getAttribute('name'),
+                ...state,
+            }),
+        })
+        .then(() => navigate(form.getAttribute('action')))
+        .catch((error) => alert(error))
+    }
+
+    return (
+        <div>
+            <span id="anchor-contact" className="section-spacing-anchor" />
+            <div id="contact" className="container">
+                <div className="tile is-ancestor tile-margin-override">
+                    <div className="tile is-child box border-radius-override">
+                        <div className="content has-text-centered">
+                            <h1 className="title"><span className="shadow-is-yellow">Contact</span></h1>
+                        </div>
+                        <hr />
+                        <div className="container container-padding">
+                            <div className="columns">
+                                <div className="column is-two-thirds">
+                                    <form name="contact" method="post" action="/thanks" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={handleSubmit}>
+                                        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                                        <input type="hidden" name="form-name" value="contact" />
+                                        <div className="field is-horizontal">
+                                            <div className="field-body">
+                                                <div className="field">
+                                                    <p className="control has-icons-left">
+                                                        <input aria-label="Name" className="input border-radius-override" id="Name" name="Name" placeholder="Name" type="text" onChange={handleChange} />
+                                                        <span className="icon is-small is-left">
+                                                            <FontAwesomeIcon icon="user" />
+                                                        </span>
+                                                    </p>
+                                                </div>
+                                                <div className="field">
+                                                    <p className="control has-icons-left has-icons-right">
+                                                        <input asp-for="Email" aria-label="Email" className="input border-radius-override" id="Email" name="Email" placeholder="Email" type="text" onChange={handleChange} />
+                                                        <span className="icon is-small is-left">
+                                                            <FontAwesomeIcon icon="envelope" />
+                                                        </span>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="field is-horizontal">
+                                            <div className="field-body">
+                                                <div className="field">
+                                                    <div className="control">
+                                                        <textarea aria-label="Message" className="textarea border-radius-override" rows={6} cols={20} id="Message" name="Message" placeholder="Message" defaultValue={""} onChange={handleChange} />
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="field is-horizontal">
-                                                <div className="field-body">
-                                                    <div className="field">
-                                                        <div className="control">
-                                                            <textarea asp-for="Message" aria-label="Message" className="textarea border-radius-override" rows={6} cols={20} id="Message" name="Message" placeholder="Message" defaultValue={""} />
-                                                        </div>
+                                        </div>
+                                        <div className="field is-horizontal">
+                                            <div className="field-body">
+                                                <div className="field">
+                                                    <div className="control">
+                                                        <input type="submit" defaultValue="Send" name="submit" className="button is-primary border-radius-override" />
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="field is-horizontal">
-                                                <div className="field-body">
-                                                    <div className="field">
-                                                        <div className="control">
-                                                            <input type="submit" defaultValue="Send" name="submit" className="button is-primary border-radius-override" />
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </form>
-                                    </div>
-                                    {/* Social -*/}
-                                    <div className="column is-one-third">
-                                        <div className="content has-text-centered">
-                                            <h1 className="title"><span className="shadow-is-red">Let's Get Social</span></h1>
-                                            <div className="buttons social is-centered">
-                                                <a href="https://twitter.com/freddie2025" className="button is-outlined social-button-twitter border-radius-override">
-                                                    <span><FontAwesomeIcon icon={['fab', 'twitter']} />&nbsp; Twitter</span>
-                                                </a>
-                                                <a href="https://github.com/freddie2025" className="button social-button-github border-radius-override">
-                                                    <span><FontAwesomeIcon icon={['fab', 'github']} />&nbsp; GitHub</span>
-                                                </a>
-                                                <a href="https://www.linkedin.com/in/freddie2025" className="button social-button-linkedin border-radius-override">
-                                                    <span><FontAwesomeIcon icon={['fab', 'linkedin-in']} />&nbsp; Linkedin</span>
-                                                </a>
-                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                                {/* Social -*/}
+                                <div className="column is-one-third">
+                                    <div className="content has-text-centered">
+                                        <h1 className="title"><span className="shadow-is-red">Let's Get Social</span></h1>
+                                        <div className="buttons social is-centered">
+                                            <a href="https://twitter.com/freddie2025" className="button is-outlined social-button-twitter border-radius-override">
+                                                <span><FontAwesomeIcon icon={['fab', 'twitter']} />&nbsp; Twitter</span>
+                                            </a>
+                                            <a href="https://github.com/freddie2025" className="button social-button-github border-radius-override">
+                                                <span><FontAwesomeIcon icon={['fab', 'github']} />&nbsp; GitHub</span>
+                                            </a>
+                                            <a href="https://www.linkedin.com/in/freddie2025" className="button social-button-linkedin border-radius-override">
+                                                <span><FontAwesomeIcon icon={['fab', 'linkedin-in']} />&nbsp; Linkedin</span>
+                                            </a>
                                         </div>
                                     </div>
                                 </div>
@@ -81,8 +109,6 @@ class Contact extends Component {
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
-
-export default Contact;
---
+++ src/pages/thanks.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Thanks = () => (
+  <Layout>
+    <SEO title="Thanks" />
+    <h1>Thanks</h1>
+    <p>You just sent an email.</p>
+  </Layout>
+)
+
+export default Thanks
